a8b1707165455d428a3fa82a325e14db31d4b496
GiancarloStabler <148743892+GiancarloStabler@users.noreply.github.com>
2025-01-02 15:07:07 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Giancarlo Stabler's Notes",
+    pageTitle: "My Math Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
